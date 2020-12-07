8861590b7c126a2cf1cc71016c528dfa790c8d96
QmelZ <qmelzthevoid@gmail.com>
2020-12-07 17:28:51 +0300

    still loading...

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 Events.on(ClientLoadEvent, () => {
-    Vars.ui.loadfrag.show();
+    Packages.arc.util.Timer.schedule(() => {
+        Vars.ui.loadfrag.show();
+        var loadTime = Mathf.random(15);
+        Packages.arc.util.Timer.schedule(() => {
+            Vars.ui.loadfrag.hide();
+        }, loadTime);
+    }, 15, 30)
 });
